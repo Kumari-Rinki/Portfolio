e7ba47bff88e0465e3d46130b432ec33082989cf
Neeraj Kumar <nk7075034@gmail.com>
2022-06-14 14:06:34 +0530

    Added animation to navbar to hide when scroll down and to appear when scroll up, also added back to top button to return to the top menu after scrolling the height of the navbar

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,3 +1,8 @@
+// Navbar appearing animation when hidden on scrolling
+let navbar = document.getElementById("nav-bar");
+let mybutton = document.getElementById("my-btn");
+let lastScrollTop = Number.NEGATIVE_INFINITY;
+
 // To create parallax scrolling effect in the background of main div
 document.body.onscroll = () => {
     let scrolltotop = document.scrollingElement.scrollTop;
@@ -6,4 +11,30 @@ document.body.onscroll = () => {
     let factor = 0.5;
     let yvalue = scrolltotop * factor;
     target.style.backgroundPosition = xvalue + " " + yvalue + "px";
+
+    // For navbar
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    let height = navbar.offsetHeight;
+    //This line will get the location on scroll
+
+    if (scrollTop > lastScrollTop) {
+        navbar.style.top = -height + "px";
+    }
+
+    else {
+        navbar.style.top = "0px";
+    }
+    lastScrollTop = scrollTop;
+
+    // For Back to top button
+    if (document.body.scrollTop > height || document.documentElement.scrollTop > height) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
 }
+
+mybutton.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
